49e0d43a9c8308ed1486b77d735d6b1af389947a
eugenetjoaa <113502655+eugenetjoaa@users.noreply.github.com>
2022-09-14 14:46:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>login form</title>
+  <link rel="stylesheet" href="style.cas" type ="text/css">
+</head>
+<body>
+  
+  <!-- <h2>login form </h2> -->
+ 
+<form action="tampil.php" method="POST">
+<h2>form login </h2>
+<div class="imgcintainer">
+  <img src="icon.png" alt="avatar" class="avatar">
+  </div>
+ 
+<div class="container">
+  <label><b>Username</b></label>
+  <input type="text" placeholder="masukkan Username"name="username" required>
+  
+  <label.<b>password</b></label>
+  <input type+"passwrd" placeholder="Masukkan Password"Name="email" required>
+  
+  <button type="submit">masuk</button>
+  <input type="checkbox" checked="checked"><span>ingat saya</spam>
+  <div>
+    
+  <div class="container" style="background-color:flflfl">
+    <button type="button" class="cancelbtn">batal</button>
+    <span class="psw">lupa <a href="#">password?</a></spam>
+  <div>
+  <form>
+  
+    </body>
+    </html>
